e36d17328bbad09a08fb51ef3116564642036ef9
Kalpin <kalpin.gajjar@bacancy.com>
2022-06-06 17:21:47 +0530

    Add assignment for file upload, action storage, associations

---
+++ app/models/book.rb
@@ -0,0 +1,6 @@
+class Book < ApplicationRecord
+    has_many_attached :cover_photos
+    belongs_to :author
+
+    validates :cover_photos, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: {less_than_or_equal_to: 1.megabytes, message: 'should be less than 1 MB'}, aspect_ratio: :square
+end
